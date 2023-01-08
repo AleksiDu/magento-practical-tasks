60dc06dc1953074e1c5eb3671b18edb261f24745
AleksiDu <aleks@Z-book>
2023-01-08 20:08:08 +0400

    add valdation rules

--- Alliance/Linkedin/Setup/InstallData.php
+++ Alliance/Linkedin/Setup/InstallData.php
@@ -1,63 +1,66 @@
 <?php
-namespace AlLiance\Linkedin\Setup;
+namespace Alliance\Linkedin\Setup;
 
 use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Eav\Model\Config;
 
+/**
+ * @codeCoverageIgnore
+ */
 class InstallData implements InstallDataInterface
 {
     /**
-     * @var EavSetup
+     * EAV setup factory
+     *
+     * @var EavSetupFactory
      */
-    private $eavSetup;
-    /**
-     * @var Config
-     */
-    private $eavConfig;
+    private $eavSetupFactory;
 
     /**
-     * @param EavSetup $eavSetup
-     * @param Config $config
+     * Init
+     *
+     * @param EavSetupFactory $eavSetupFactory
      */
-    public function __construct(EavSetup $eavSetup, Config $config){
-        $this->eavSetup = $eavSetup;
-        $this->eavConfig = $config;
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
     }
 
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        $setup->startSetup();
-        $this->eavSetup->addAttribute(
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        /**
+         * Add attributes to the eav/attribute
+         */
+        $eavSetup->addAttribute(
             \Magento\Customer\Model\Customer::ENTITY,
             'linkedin_profile',
             [
                 'type' => 'varchar',
-                'label' => 'Linkedin Profile',
+                'label' => 'LinkedIn Profile',
                 'input' => 'text',
                 'required' => false,
                 'visible' => true,
+                'user_defined' => true,
                 'sort_order' => 1000,
                 'position' => 1000,
                 'system' => false,
+                'validate_rules' => [
+                    'max_text_length' => 250,
+                    'min_text_length' => 1,
+                    'input_validation' => 'url',
+                    'unique' => true
+                ],
             ]
         );
-
-        $linkedin = $this->eavConfig->getAttribute(
-            \Magento\Customer\Model\Customer::ENTITY,
-            'linkedin_profile');
-
-        $linkedin->setData(
-            'used_in_forms',
-            ['adminhtml_customer', 'adminhtml_checkout', 'customer_account_create', 'customer_account_edit']
-        );
-
-        $linkedin->save();
-
-        $setup->endSetup();
     }
-
 }
-
